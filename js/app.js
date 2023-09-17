45fbe3da7503675c9054ea782506100967ddb142
unknown <ng.tanduy261203@gmail.com>
2023-09-17 14:29:03 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,79 +1,79 @@
-$(document).ready(function() {
-  $('.product-detail_thumb').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.product-detail_gallery',
-  });
-  $('.product-detail_gallery').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.product-detail_thumb',
-    dots: false,
-    centerMode: true,
-    focusOnSelect: true,
-    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
-      responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-  $('.product-relate_item').slick({
-    slidesToShow: 4,
-    arrows: true,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    centerMode: true,
-    centerPadding: '40px',
-    prevArrow:"<button type='button' class='arrow_prev slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-    nextArrow:"<button type='button' class='arrow_next slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
-    responsive: [
-      {
-        breakpoint: 1000,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '20px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+$(document).ready(function () {
+    $('.product-detail_thumb').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.product-detail_gallery',
+    });
+    $('.product-detail_gallery').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.product-detail_thumb',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+    $('.product-relate_item').slick({
+        slidesToShow: 4,
+        arrows: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        centerMode: true,
+        centerPadding: '40px',
+        prevArrow: "<button type='button' class='arrow_prev slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='arrow_next slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '20px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 })
